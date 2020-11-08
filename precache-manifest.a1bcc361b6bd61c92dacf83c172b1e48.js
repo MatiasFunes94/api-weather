bfde295c9508849c241248acb55b20d955885789
Matias Funes <matiasfunes94@gmail.com>
2020-11-08 20:46:32 -0300

    Updates

---
+++ precache-manifest.a1bcc361b6bd61c92dacf83c172b1e48.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d37ad668216301ed100c81944376862",
+    "url": "/api-weather/index.html"
+  },
+  {
+    "revision": "921f80c260645d76bbcc",
+    "url": "/api-weather/static/css/main.bc6ef857.chunk.css"
+  },
+  {
+    "revision": "9ccc9d7395aefc13586f",
+    "url": "/api-weather/static/js/2.d9b277e5.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/api-weather/static/js/2.d9b277e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "921f80c260645d76bbcc",
+    "url": "/api-weather/static/js/main.0be582cc.chunk.js"
+  },
+  {
+    "revision": "290fb3a4dc605701ceee",
+    "url": "/api-weather/static/js/runtime-main.26441e77.js"
+  }
+]);
